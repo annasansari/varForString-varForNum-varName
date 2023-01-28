ace938c655572ef89ce95657a8e7b7608a15542e
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-01-28 13:31:50 -0800

    Add files via upload

---
+++ JS Assignment # 2/app.js
@@ -0,0 +1,75 @@
+// *********** Assignment # 2 ***********
+// ******** ChAPTER # 2 ********
+
+// Question # 1
+// var username
+
+// Question # 2
+// var myName = "Annas ansari"
+
+// Question # 3
+// var message = 'Hello World'
+// alert(message)
+
+// Question # 4
+// var name = 'Anas'
+// alert(name)
+
+// var age = '20'
+// alert(age)
+
+// var course = 'Web & App Development Course'
+// alert(course)
+
+
+// ******** ChAPTER # 3 ********
+
+// Question # 1
+// var age = '20'
+// alert(" I'm " + age + " years old ")
+
+// Question # 2
+// var visitor = 14;
+// alert(" You have Visited this site " + visitor + " times. ")
+
+//Question # 3 
+// var birthYear = 2002;
+// document.write("My birth  Year is " + birthYear)
+// document.write(" \n and data type that i declare above is number")
+
+// Question # 4
+// var name = 'Anas'
+// var title = 'T-Shirt'
+// var quantity = '4'
+
+// document.write(name + " Ordered " + quantity + title)
+// document.write(" from anasClothing.com ")
+
+
+// ******** ChAPTER # 4 ********
+
+//Question # 1
+// var name, age, gender
+
+//Question # 2
+// Legal Variables
+
+// *** var name ***
+
+// var your_age
+// var $gender
+// var my1name
+// var humTum
+
+// ***illegal Vaiables *** 
+
+// var var
+// var my name
+// var 'hello'
+// var 1name
+// var mob.ile
+
+
+// Question # 3
+
+// document.write("<h1>Rules for naming JS Variables</h1>" + "<br>" + "Variable names can only contain numbers, $ and _ .For example $name, _name or name" + "<br>" + "For example $name, _name or name" + "<br>" + "Variable names are case sensitive" + "<br>" + "Variable names should not be JS keyword")
